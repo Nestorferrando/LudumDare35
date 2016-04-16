066298000b8f026f3721da060c277cbef389fffe
Néstor Ferrando <nestor.ferrando@photoniktech.com>
2016-04-16 21:41:27 +0200

    added FacesDatabase

---
+++ Shapeshift Agent/Assets/scripts/model/FacesDatabase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+    public class FacesDatabase
+    {
+
+    public static Face SampleFace = new Face(
+            new FacePart(PartType.LEFT_EYE, 0, 0, 0),
+            new FacePart(PartType.RIGHT_EYE, 0, 0, 0),
+            new FacePart(PartType.NOSE, 0, 0, 0),
+            new FacePart(PartType.MOUTH, 0, 0, 0),
+            new FacePart(PartType.CONTOUR, 0, 0, 0),
+            new FacePart(PartType.HAIR, 0, 0, 0),
+            HairColor.GINGER,
+            SkinColor.WHITE
+            );
+
+}
+
