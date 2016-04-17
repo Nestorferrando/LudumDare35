e44bdf5bf6109e296f5e76f5464f750b88d8139b
Néstor Ferrando <nestor.ferrando@photoniktech.com>
2016-04-17 22:04:07 +0200

    added TrustController

--- Shapeshift Agent/Assets/SingletonData.cs
+++ Shapeshift Agent/Assets/SingletonData.cs
@@ -6,7 +6,7 @@ public class SingletonData
 
     private static Face _CurrrentFace = FacesDatabase.Subject5;
     private static int currentInfiltrationLevel;
-    private static Target current;
+    private static Target _currentTarget = new Target(TargetConfidence.FOUR);
 
 
 
@@ -20,5 +20,12 @@ public class SingletonData
     {
         get { return currentInfiltrationLevel; }
         set { currentInfiltrationLevel = value; }
+    }
+
+
+    public static Target CurrentTarget
+    {
+        get { return _currentTarget; }
+        set { _currentTarget = value; }
     }
 }
---
+++ Shapeshift Agent/Assets/TrustController.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrustController : MonoBehaviour
+{
+
+    private Target previousTarget;
+
+    private GameObject title;
+    private GameObject bar0;
+    private GameObject bar1;
+    private GameObject bar2;
+    private GameObject bar3;
+    private GameObject bar4;
+
+
+    // Use this for initialization
+    void Start () {
+	    title = GameObject.Find("trustTitle");
+        bar0 = GameObject.Find("trustbar0");
+        bar1 = GameObject.Find("trustbar1");
+        bar2 = GameObject.Find("trustbar2");
+        bar3 = GameObject.Find("trustbar3");
+        bar4 = GameObject.Find("trustbar4");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	    if (previousTarget == null || SingletonData.CurrentTarget != previousTarget)
+	    {
+	        updateTrustGraphics(SingletonData.CurrentTarget);
+
+	        previousTarget = SingletonData.CurrentTarget;
+
+	    }
+
+	}
+
+    private void updateTrustGraphics(Target currentTarget)
+    {
+        switch (currentTarget.Confidence)
+
+        {
+            case TargetConfidence.ZERO:
+                title.GetComponent<SpriteRenderer>().color = new Color(1f, 0.0f, 0, 1);
+                bar0.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+                bar1.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+                bar2.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+                bar3.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+                bar4.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+                break;
+
+            case TargetConfidence.ONE:
+                title.GetComponent<SpriteRenderer>().color = new Color(1f, 0.0f, 0, 1);
+                bar0.GetComponent<SpriteRenderer>().color = new Color(1f, 0.0f, 0, 1);
+                bar1.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+                bar2.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+                bar3.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+                bar4.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+                break;
+
+            case TargetConfidence.TWO:
+                title.GetComponent<SpriteRenderer>().color = new Color(1f, 0.5f, 0, 1);
+                bar0.GetComponent<SpriteRenderer>().color = new Color(1f, 0.5f, 0, 1);
+                bar1.GetComponent<SpriteRenderer>().color = new Color(1f, 0.5f, 0, 1);
+                bar2.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+                bar3.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+                bar4.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+                break;
+
+            case TargetConfidence.THREE:
+                title.GetComponent<SpriteRenderer>().color = new Color(1f, 0.5f, 0, 1);
+                bar0.GetComponent<SpriteRenderer>().color = new Color(1f, 0.5f, 0, 1);
+                bar1.GetComponent<SpriteRenderer>().color = new Color(1f, 0.5f, 0, 1);
+                bar2.GetComponent<SpriteRenderer>().color = new Color(1f, 0.5f, 0, 1);
+                bar3.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+                bar4.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+                break;
+
+            case TargetConfidence.FOUR:
+                title.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0, 1);
+                bar0.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0, 1);
+                bar1.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0, 1);
+                bar2.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0, 1);
+                bar3.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0, 1);
+                bar4.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+                break;
+
+            case TargetConfidence.FIVE:
+                title.GetComponent<SpriteRenderer>().color = new Color(0,1,0,1);
+                bar0.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0, 1);
+                bar1.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0, 1);
+                bar2.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0, 1);
+                bar3.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0, 1);
+                bar4.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0, 1);
+                break;
+
+
+        }
+    }
+}
