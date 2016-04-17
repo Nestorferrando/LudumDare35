013d1fa86b660ce6106bc934adbdd610d0b3f71a
Guillermo Ferrando <guillermofm7@gmail.com>
2016-04-17 11:47:15 +0200

    dialogue tag system done

--- Shapeshift Agent/Assets/scripts/DialogText.cs
+++ Shapeshift Agent/Assets/scripts/DialogText.cs
@@ -15,8 +15,10 @@ public class DialogText : MonoBehaviour {
     private Queue<string> queuedText;
     private Text text;
     private string lastPeak = "";
+    private string missionText = "";
+    private const int max_lines = 3;
 
-    private enum State { NONE, DRAWALL, NEXT }
+    private enum State { NONE, DRAWALL, NEXT, QUESTION }
     private State currentState = State.NONE;
 
 
@@ -30,7 +32,8 @@ public class DialogText : MonoBehaviour {
         text = gameObject.GetComponent<Text>();
         string s = text.text;
         text.text = "";
-        queuedText = processText(s);
+        missionText = Resources.Load<TextAsset>("SceneText/mission1").text;
+        queuedText = processText(missionText);
         displayNextDialog();
     }
 
@@ -45,20 +48,39 @@ public class DialogText : MonoBehaviour {
         sr.sprite = s;
     }
 
-    static IEnumerable<string> splitText(string str, int chunkSize) {
-        return Enumerable.Range(0, str.Length / chunkSize).Select(i => str.Substring(i * chunkSize, chunkSize));
+
+    string[] currentTags = { "FAILURE", "ALWAYS", "SILLY" };
+    public bool checkTags(string tag) {
+        foreach (string t in currentTags) {
+            //Debug.Log(t + " =?= " + tag + " | " + t==tag + " | " + t.Length + " " + tag.Length);
+            if (t == tag) {
+                return true;
+            } 
+        }
+        return false;
     }
 
     public Queue<string> processText(string text) {
         Queue<string> result = new Queue<string>();
-        /*
-        foreach (string s in splitText(text, charPerLine)) {
-            result.Enqueue(s);
-        }
-        */
-        string[] sep = { "[next]" };
-        foreach (string s in text.Split(sep, StringSplitOptions.RemoveEmptyEntries)) {
-            result.Enqueue(s);
+
+        string[] sep_tag = { "[TAG]" };
+        string[] sep_nextLine = { "[next]" };
+        foreach (string s in text.Split(sep_tag, StringSplitOptions.RemoveEmptyEntries)) {
+            string[] lineText = s.Split('\n');
+            string tagFound = lineText[1].Substring(0, lineText[1].Length-1);
+
+            if (checkTags(tagFound)) {
+
+                string res = "";
+                for (int i = 2; i < lineText.Length; ++i) {
+                    res += lineText[i] + '\n';
+                }
+
+                foreach (string t in res.Split(sep_nextLine, StringSplitOptions.RemoveEmptyEntries)) {
+                    result.Enqueue(t);
+                }
+
+            }
         }
 
         return result;
@@ -101,6 +123,8 @@ public class DialogText : MonoBehaviour {
                 currentState = State.DRAWALL;
                 displayNextDialog();
                 break;
+            case State.QUESTION:
+                displayQuestionDialog();
 
             case State.NONE:
             default:
@@ -119,6 +143,8 @@ public class DialogText : MonoBehaviour {
             drawNewText(str, strComplete[i - 1], i);
             yield return new WaitForSeconds(wait);
         }
+
+        currentState = State.NEXT;
     }
 
     private void drawEverything(string completeText) {
